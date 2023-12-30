6b047e3b4c7319810b46258784a4087959cf6711
soravi002 <gomisezekielpaulantoine@gmail.com>
2023-12-30 01:03:58 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PlayersList from './PlayerList';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>FIFA Players</h1>
+      <PlayersList />
+    </div>
+  );
+}
+
+export default App;
